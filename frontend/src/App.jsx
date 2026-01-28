dce5c52ae6d2b19ab79db7074e856f72b81c1bd0
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-01-28 11:47:18 -0700

    ProductDetails page Initial Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import ProductList from "../pages/ProductList.jsx";
+import ProductDetails from "../pages/ProductDetails.jsx";
 import React from 'react'
+
 
 const App = () => {
     return (
         <>
-            <ProductList/>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<ProductList/>}/>
+                    <Route path="/product/:id" element={<ProductDetails/>}/>
+                </Routes>
+            </Router>
         </>
     )
 }
