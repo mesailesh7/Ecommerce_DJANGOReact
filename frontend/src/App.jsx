88df94f12180eaa97c2199ba25eae1cec679656f
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-02-01 00:48:35 -0700

    Add to cart => ContextApi (Cart State) => Display Cart items in cartPage => Update Quantity => Remove Items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,18 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import ProductList from "./pages/ProductList.jsx";
 import ProductDetails from "./pages/ProductDetails.jsx";
 import React from 'react'
-
+import Navbar from "./components/Navbar.jsx";
+import CartPage from "./pages/CartPage.jsx";
 
 const App = () => {
     return (
         <>
             <Router>
+                <Navbar/>
                 <Routes>
                     <Route path="/" element={<ProductList/>}/>
                     <Route path="/product/:id" element={<ProductDetails/>}/>
+                    <Route path="/cart" element={<CartPage/>}/>
                 </Routes>
             </Router>
         </>
---
+++ frontend/src/context/CartContext.jsx
@@ -0,0 +1,43 @@
+﻿import React from 'react'
+import {createContext, useContext, useState} from "react";
+
+
+const CartContext = createContext();
+
+export const CartProvider = ({children}) => {
+    const [cartItems, setCartItems] = useState([]);
+
+    const addToCart = (product) => {
+        const existing = cartItems.find((item) => item.id === product.id);
+        if (existing) {
+            setCartItems(cartItems.map((item) => item.id === product.id ? {
+                ...item, quantity: item.quantity + 1
+            } : item));
+        } else {
+            setCartItems([...cartItems, {...product, quantity: 1}]);
+        }
+    };
+
+    const removeFromCart = (id) => {
+        setCartItems(cartItems.filter((item) => item.id !== id));
+    }
+
+    const updateQuantity = (id, quantity) => {
+        if (quantity < 1) {
+            setCartItems(
+                cartItems.map((item) => item.id == id ? {...item, quantity} : item)
+            )
+        }
+    }
+
+    return (
+        <CardContext.Provider
+            value={{cartItems, addToCart, removeFromCart, updateQuantity}}
+        >
+            {children}
+        </CardContext.Provider>
+    )
+}
+
+
+export const useCart = () => useContext(CartContext);
